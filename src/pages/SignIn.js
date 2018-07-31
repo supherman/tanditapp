3456222a0d41054373d3c04134a429e397316b79
Alexis Navarro <alexisnavarro23@gmail.com>
2018-07-31 22:20:30 +0000

    add eslint

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const SignIn = () => {
-    return(
-        <div className="container margin-auto">
-            <div className="panel small-padding white-bg small-margin-top">
-                <label>Usuario:</label>
-                <input type="text" />
-                <label>Email:</label>
-                <input type="password" />
-                <button className="button primary">Iniciar Sesión</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="container margin-auto">
+      <div className="panel small-padding white-bg small-margin-top">
+        <label>Usuario:</label>
+        <input type="text" />
+        <label>Email:</label>
+        <input type="password" />
+        <button className="button primary">Iniciar Sesión</button>
+      </div>
+    </div>
+  );
+};
 
 export default SignIn;
